1e49669a8c14987086358ecc376c1127052b1bd0
benitovillarruel <benitovillarruel01@gmail.com>
2026-05-28 14:16:45 -0500

    proyecto base

---
+++ app.py
@@ -0,0 +1,11 @@
+# Programa para sumar dos números
+
+# Pedimos al usuario que ingrese los números
+numero1 = float(input("Ingresa el primer número: "))
+numero2 = float(input("Ingresa el segundo número: "))
+
+# Realizamos la suma
+suma = numero1 + numero2
+
+# Mostramos el resultado
+print("La suma de", numero1, "y", numero2, "es:", suma)
